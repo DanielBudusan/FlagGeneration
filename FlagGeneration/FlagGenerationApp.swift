355f1a7d6ae570bf71db96deb660f4eef7fb2883
Daniel Budusan <danibudusan@gmail.com>
2024-04-16 20:37:22 +0300

    Initial Commit

---
+++ FlagGeneration/FlagGenerationApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlagGenerationApp.swift
+//  FlagGeneration
+//
+//  Created by Daniel Budusan on 16.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct FlagGenerationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
